28294c5a476555c57887d5d4bbb4f9ef3df50381
madwind76 <madwind76@gmail.com>
2015-06-17 00:52:28 +0900

    목록 출력을 위한 신규 URL, 뷰, 템플릿

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,7 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-from lists.models import Item
+from lists.models import Item, List
 
 from lists.views import home_page
 
@@ -20,59 +20,60 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode('utf-8'), expected_html)
 
-    def test_home_page_can_save_a_POST_request(self):
-        request = HttpRequest()
-        request.method = "POST"
-        request.POST['item_text'] = u'신규 작업 아이템'
 
-        response = home_page(request)
+class ListViewTest(TestCase):
 
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+
+
+class NewListTest(TestCase):
+
+    def test_save_a_POST_request(self):
+        self.client.post(
+            '/lists/new',
+            data={'item_text': u'신규 작업 아이템'}
+        )
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, u'신규 작업 아이템')
 
-    def test_home_page_redirects_after_POST(self):
-        request = HttpRequest()
-        request.method = "POST"
-        request.POST['item_text'] = u'신규 작업 아이템'
-
-        response = home_page(request)
-
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
-        # self.assertIn(u'신규 작업 아이템', response.content.decode('utf-8'))
-        # expected_html = render_to_string(
-        #     'home.html',
-        #     {'new_item_text': '신규 작업 아이템'}
-        # )
-        # self.assertEqual(response.content.decode('utf-8'), expected_html)
-
-    def test_home_page_only_saves_items_when_necessary(self):
-        request = HttpRequest()
-        home_page(request)
-        self.assertEqual(Item.objects.count(), 0)
-
-    def test_home_page_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        request = HttpRequest()
-        response = home_page(request)
-
-        self.assertIn('itemey 1', response.content.decode('utf-8'))
-        self.assertIn('itemey 2', response.content.decode('utf-8'))
+    def test_redirects_after_POST(self):
+        response = self.client.post(
+            '/lists/new',
+            data={'item_text': u'신규 작업 아이템'}
+        )
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
-        first_item = Item()
-        first_item.text = u'첫번째 아이템'
-        first_item.save()
+        list_ = List()
+        list_.save()
 
         first_item = Item()
-        first_item.text = u'두번째 아이템'
+        first_item.text = u'첫번째 아이템'
+        first_item.list = list_
         first_item.save()
+
+        second_item = Item()
+        second_item.text = u'두번째 아이템'
+        second_item.list = list_
+        second_item.save()
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -80,4 +81,6 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, u'첫번째 아이템')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, u'두번째 아이템')
+        self.assertEqual(second_saved_item.list, list_)
