0569b6fe30001b86e04ba827594ef8ed7b520f83
madwind76 <madwind76@gmail.com>
2015-06-16 17:51:31 +0900

    "템플릿을 이용한 메인 페이지 HTML 생성"

--- superlists/settings.py
+++ superlists/settings.py
@@ -86,9 +86,11 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-KR'
 
-TIME_ZONE = 'UTC'
+DEFAULT_CHARSET = 'utf-8'
+
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
